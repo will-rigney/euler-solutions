c4f8960c29bfb34acf7e9a088a2ac8120a32835f
will <will@wrign.xyz>
2021-06-06 15:48:43 +1000

    problem 2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,30 @@ use std::{u32, u64};
 
 fn main() {
     println!("sum of multiples of 3 or 5 under 1000: {}", problem_1());
+    println!(
+        "sum of even values of fibonacci sequence under 4 million: {}",
+        problem_2()
+    );
+}
+
+fn problem_2() -> u64 {
+    // sum of even values of fibonacci sequence under 4 million:
+    let mut sum = 0;
+    let mut a = 1;
+    let mut b = 1;
+    let mut result = 0;
+    while result < 4000000 {
+        result = a + b;
+        a = b;
+        b = result;
+
+        // check a number is even
+        if (result & 1) == 0 {
+            sum += result;
+        }
+    }
+
+    sum
 }
 
 fn problem_1() -> u64 {
