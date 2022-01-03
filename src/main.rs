da4113d68c89f41219a364c2ee302ce7ba73793b
will <william@rigney.com.au>
2022-01-03 20:50:17 +1100

    argument parsing to print one problem at a time

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,83 @@
 mod print;
 mod problems_11_20;
 mod problems_1_10;
+use clap::Parser;
 use print::*;
 use problems_11_20::*;
 use problems_1_10::*;
 
+#[derive(Parser, Debug)]
+#[clap(
+    about = "Solutions to problems at https://projecteuler.net/ implemented in Rust.",
+    version = "0.0.0",
+    author = "Will Rigney"
+)]
+
+struct Args {
+    /// Specify the problem to solve; defaults to 0 which means all problems.
+    #[clap(short, long, default_value_t = 0)]
+    problem: u64,
+}
+
 fn main() {
-    print_heading("Euler Project Solutions");
+    let args = Args::parse();
 
-    print_problem("1. sum of multiples of 3 or 5 under 1000:", problem_1());
-    print_problem(
-        "2. sum of even values of fibonacci sequence under 4 million:",
-        problem_2(),
-    );
-    print_problem("3. largest prime factor of 600851475143:", problem_3());
-    print_problem(
-        "4. largest palindrome number that is product of two 3 digit numbers:",
-        problem_4(),
-    );
-    print_problem(
-        "5. smallest positive number divisible by all numbers from 1 to 20:",
-        problem_5(),
-    );
-    print_problem(
-        "6. difference between sum of squares and square of sum of numbers from 1 to 100:",
-        problem_6(),
-    );
-    print_problem("7. 10001st prime:", problem_7());
-    print_problem(
-        "8. product of 13 adjacent digits in the 1000 digit number with the greatest product:",
-        problem_8(),
-    );
-    print_problem(
-        "9. product of pythagorean triple whose sum is 1000:",
-        problem_9(),
-    );
-    print_problem("10. sum of all primes under 2 million:", problem_10());
+    let n_problems = 16;
 
-    print_problem(
-        "11. greatest product of 4 numbers in each direction of 20 x 20 array:",
-        problem_11(),
-    );
+    // problems to solve
+    let problems = match args.problem {
+        0 => {
+            print_heading("Euler Project Solutions");
+            (1..=n_problems).collect()
+        }
+        _ => vec![args.problem],
+    };
 
-    print_problem(
-        "12. first triangle number with over 500 divisors:",
-        problem_12(),
-    );
-
-    print_problem(
-        "14. starting number of longest collatz sequence under one million:",
-        problem_14(),
-    );
-
-    print_problem("15. lattice paths in 20x20 grid:", problem_15());
-
-    print_problem("16. sum of digits in 2^1000:", problem_16());
+    for problem in problems {
+        match problem {
+        1 => print_problem("1. sum of multiples of 3 or 5 under 1000:", problem_1()),
+        2 => print_problem(
+            "2. sum of even values of fibonacci sequence under 4 million:",
+            problem_2(),
+        ),
+        3 => print_problem("3. largest prime factor of 600851475143:", problem_3()),
+        4 => print_problem(
+            "4. largest palindrome number that is product of two 3 digit numbers:",
+            problem_4(),
+        ),
+        5 => print_problem(
+            "5. smallest positive number divisible by all numbers from 1 to 20:",
+            problem_5(),
+        ),
+        6 => print_problem(
+            "6. difference between sum of squares and square of sum of numbers from 1 to 100:",
+            problem_6(),
+        ),
+        7 => print_problem("7. 10001st prime:", problem_7()),
+        8 => print_problem(
+            "8. product of 13 adjacent digits in the 1000 digit number with the greatest product:",
+            problem_8(),
+        ),
+        9 => print_problem(
+            "9. product of pythagorean triple whose sum is 1000:",
+            problem_9(),
+        ),
+        10 => print_problem("10. sum of all primes under 2 million:", problem_10()),
+        11 => print_problem(
+            "11. greatest product of 4 numbers in each direction of 20 x 20 array:",
+            problem_11(),
+        ),
+        12 => print_problem(
+            "12. first triangle number with over 500 divisors:",
+            problem_12(),
+        ),
+        14 => print_problem(
+            "14. starting number of longest collatz sequence under one million:",
+            problem_14(),
+        ),
+        15 => print_problem("15. lattice paths in 20x20 grid:", problem_15()),
+        16 => print_problem("16. sum of digits in 2^1000:", problem_16()),
+        _ => print_missing(format!("no solution for problem {}", problem))
+        }
+    }
 }
--- src/print.rs
+++ src/print.rs
@@ -3,11 +3,16 @@ use ansi_term::Style;
 use std::fmt::Display;
 
 pub fn print_heading(s: &str) {
-    let h_style: ansi_term::Style = Style::new().bold().fg(Colour::Blue);
+    let h_style = Style::new().bold().fg(Colour::Blue);
     println!("{}", h_style.paint(s));
 }
 
 pub fn print_problem<T: Display>(q: &str, a: T) {
-    let style: ansi_term::Style = Style::new().bold().fg(Colour::Purple);
+    let style = Style::new().bold().fg(Colour::Purple);
     println!("{} {}", q, style.paint(format!("{}", a)));
+}
+
+pub fn print_missing<T: Display>(s: T) {
+    let style = Style::new().bold().fg(Colour::Red);
+    println!("{}", style.paint(format!("{}", s)));
 }
